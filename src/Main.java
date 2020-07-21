b2e70a1dd3baeff42808f725d4584548305fd6b0
Malypko Maxim <malypko@inbox.ru>
2020-07-21 17:03:55 +0400

    Cherry-pick

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,31 @@ public class Main {
     System.out.println(dev.quotient(4,2));
     System.out.println(dev.sum(2,2));
   }
+
+  protected double balance = 0;
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  protected void withdrawMoney(double diff) {
+    balance = balance - diff;
+    System.out.println("Произошло списание " + diff);
+  }
+
+  protected void depositMoney(double sum) {
+    balance = balance + sum;
+    System.out.println("Произошло зачисление " + sum);
+  }
+
+  protected abstract void infoAccount();
+
+  protected abstract double getWithdrawalComission(double amount, double percent);
+
+  protected abstract double getDepositComission(double amount, double percent);
+  
 }
